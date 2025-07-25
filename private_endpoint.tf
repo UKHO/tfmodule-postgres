b6c9d33498e203d4267228017a7ee3b0670b2756
richl-ukho <richard.lang@ukho.gov.uk>
2025-07-25 08:23:23 +0100

    add destroy

--- private_endpoint.tf
+++ private_endpoint.tf
@@ -24,6 +24,12 @@ resource "terraform_data" "app_routing" {
   ]
 
   provisioner "local-exec" {
+    when    = create
     command = "az aks approuting enable -n ${azurerm_postgresql_flexible_server.this.name} -g ${var.resource_group_name}"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "az aks approuting disable -n ${azurerm_postgresql_flexible_server.this.name} -g ${var.resource_group_name}"
   }
 }
