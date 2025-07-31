0a1debc90224764af392fe46b8b153725c6db294
richl-ukho <richard.lang@ukho.gov.uk>
2025-07-31 15:02:23 +0100

    dont bother with locals

--- locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  dns_zone_name = "privatelink.postgres.database.azure.com"
-  zone_group = "private-dns-zone-group"
-}
--- private_endpoint.tf
+++ private_endpoint.tf
@@ -14,8 +14,8 @@ module "private_endpoint" {
   pe_resource_group           = [var.resource_group_name]
   pe_resource_group_locations = [var.location]
   dns_resource_group          = var.dns_resource_group_name
-  zone_group                  = local.zone_group
-  dns_zone                    = local.dns_zone_name
+  zone_group                  = "private-dns-zone-group"
+  dns_zone                    = "privatelink.postgres.database.azure.com"
   subresource_names           = ["postgresqlServer"]
 
   count = var.pe_enabled ? 1 : 0
