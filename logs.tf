f30d110d9b592f70209fb82228112a26ff9f61b4
Paul King <6100643+gyro2009@users.noreply.github.com>
2026-09-03 20:33:21 +0100

    Disable connection_throttle for postgres 18+

--- logs.tf
+++ logs.tf
@@ -10,6 +10,8 @@ resource "azurerm_postgresql_flexible_server_configuration" "postgres_log_connec
 }
 
 resource "azurerm_postgresql_flexible_server_configuration" "postgres_log_connection_throttling" {
+  count     = var.postgres_version < 18 ? 1 : 0
+
   name      = "connection_throttle.enable"
   server_id = azurerm_postgresql_flexible_server.this.id
   value     = "on"
