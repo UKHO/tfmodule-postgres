dd0409e8d41b3eb69b99ed51e33fe891f440d7b4
Paul King <6100643+gyro2009@users.noreply.github.com>
2026-09-04 12:05:37 +0100

    Fix formatting

--- logs.tf
+++ logs.tf
@@ -15,7 +15,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "postgres_log_connec
 
 resource "azurerm_postgresql_flexible_server_configuration" "postgres_log_connection_throttling" {
   depends_on = [azurerm_postgresql_flexible_server.this]
-  count     = var.postgres_version < 18 ? 1 : 0
+  count      = var.postgres_version < 18 ? 1 : 0
 
   name      = "connection_throttle.enable"
   server_id = azurerm_postgresql_flexible_server.this.id
