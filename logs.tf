8093b92d7bc12b02e8b3888f3aa93a22a50cde6f
Paul King <6100643+gyro2009@users.noreply.github.com>
2026-09-04 12:04:50 +0100

    Wait for main resource to be created to prevent errors

--- logs.tf
+++ logs.tf
@@ -1,15 +1,20 @@
 resource "azurerm_postgresql_flexible_server_configuration" "postgres_log_checkpoints" {
+  depends_on = [azurerm_postgresql_flexible_server.this]
+
   name      = "log_checkpoints"
   server_id = azurerm_postgresql_flexible_server.this.id
   value     = "on"
 }
 resource "azurerm_postgresql_flexible_server_configuration" "postgres_log_connections" {
+  depends_on = [azurerm_postgresql_flexible_server.this]
+
   name      = "log_connections"
   server_id = azurerm_postgresql_flexible_server.this.id
   value     = "on"
 }
 
 resource "azurerm_postgresql_flexible_server_configuration" "postgres_log_connection_throttling" {
+  depends_on = [azurerm_postgresql_flexible_server.this]
   count     = var.postgres_version < 18 ? 1 : 0
 
   name      = "connection_throttle.enable"
